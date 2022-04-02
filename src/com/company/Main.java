d24cfe916e1bdf3f738a263aa7b6710cf881f9ac
DanyloKovalchyk <101935665+DanyloKovalchyk@users.noreply.github.com>
2022-04-02 11:15:12 +0300

    Add files via upload

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int a, b, c, d;
+        a=1;
+        b=2;
+        c=5;
+        d=10;
+
+        System.out.println(10/a);
+        System.out.println(10/b);
+        System.out.println(10/c);
+        System.out.println(10/d);
+    }
+}
